c59d8a503f1a35169cd0d368bc5a877d764ece30
Yannick R. Brodard <yannick.r.brodard@gmail.com>
2015-05-12 09:00:42 +0200

    Added sauce to camera class

--- src/HelProject/UI/Camera.cs
+++ src/HelProject/UI/Camera.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿/*
+ * Author : Yannick R. Brodard
+ * File name : Camera.cs
+ * Version : 0.1.201505120855
+ * Description : Camera class for the screen
+ */
+
+using Microsoft.Xna.Framework;
 
 namespace HelProject.UI
 {
+    /// <summary>
+    /// Camera class for the screen
+    /// </summary>
     public class Camera
     {
         private float _zoom;
@@ -13,18 +19,33 @@ namespace HelProject.UI
         private int _width;
         private int _height;
 
+        /// <summary>
+        /// Height of the camera
+        /// </summary>
+        /// <remarks>
+        /// Usually the height of the window of the game
+        /// </remarks>
         public int Height
         {
             get { return _height; }
             set { _height = value; }
         }
 
+        /// <summary>
+        /// Width of the camera
+        /// </summary>
+        /// <remarks>
+        /// Usually the width of the window of the game
+        /// </remarks>
         public int Width
         {
             get { return _width; }
             set { _width = value; }
         }
 
+        /// <summary>
+        /// Gets the view port of the camera
+        /// </summary>
         public Rectangle ViewPort
         {
             get
@@ -34,18 +55,31 @@ namespace HelProject.UI
             }
         }
 
+        /// <summary>
+        /// Position of the camera, relative to the map
+        /// </summary>
         public Vector2 Position
         {
             get { return _position; }
             set { _position = value; }
         }
 
+        /// <summary>
+        /// Zoom effect
+        /// </summary>
         public float Zoom
         {
             get { return _zoom; }
             set { _zoom = value; }
         }
 
+        /// <summary>
+        /// Create a camera
+        /// </summary>
+        /// <param name="position">Position of the camera</param>
+        /// <param name="width">Width of the camera</param>
+        /// <param name="height">Height of the camera</param>
+        /// <param name="zoom">Zoom of the camera</param>
         public Camera(Vector2 position, int width, int height, float zoom = 1.0f)
         {
             this.Position = position;
